ecaae4ac3f26ab0b29176694d9b70539c73e4436
shotgunmech <47787927+shotgunmech@users.noreply.github.com>
2020-10-27 14:04:52 -0400

    initial commit after creation of react portfolio with animations and logo designed

---
+++ components/react-logo/react-logo.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './react-logo.scss';
+
+const ReactLogo = () => {
+  return (
+<div class="holder">
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+</div>
+  );
+};
+
+export default ReactLogo;
